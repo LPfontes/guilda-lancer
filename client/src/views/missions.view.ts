ba5c30a21dfa68b27b983fd295557f827af0ce55
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2026-09-10 18:14:20 -0300

    fix pattern

--- client/src/views/missions.view.ts
+++ client/src/views/missions.view.ts
@@ -609,7 +609,7 @@ export class MissionsView {
                     id="mission-start-date-input"
                     class="form-input"
                     placeholder="DD/MM/AAAA"
-                    pattern="^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$"
+                    pattern="(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/[0-9]{4}"
                     title="Formato de data brasileiro: DD/MM/AAAA"
                     maxlength="10"
                     inputmode="numeric"
